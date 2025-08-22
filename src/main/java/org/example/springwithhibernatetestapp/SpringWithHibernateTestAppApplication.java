6aa353a5707f4cf3af0b6fd74c0e93e07b279ec8
atandon2024 <ajay.tandon@mongodb.com>
2025-08-22 12:50:44 -0700

    basic hello get call

---
+++ src/main/java/org/example/springwithhibernatetestapp/SpringWithHibernateTestAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.springwithhibernatetestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithHibernateTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithHibernateTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springwithhibernatetestapp/SpringWithHibernateTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springwithhibernatetestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithHibernateTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
